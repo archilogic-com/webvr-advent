eed56a0033e370bce226ffee1a7843bd8f810634
Martin Splitt <mr.avgp@googlemail.com>
2017-12-01 16:43:30 +0100

    Initial commit, day 1

---
+++ script.js
@@ -0,0 +1,37 @@
+var sceneId = 'ce59ff59-2b1a-4642-825f-f0c2c53485cc'
+var scene = document.querySelector('a-scene')
+var user = document.getElementById('head')
+var intro = document.getElementById('introPanel')
+var teleporter = document.getElementById('teleporter')
+
+io3d.scene.getAframeElements(sceneId).then(function (elements) {
+  elements
+    .filter(function (elem) { return !elem.hasAttribute('camera') })
+    .forEach(function (elem) {
+      elem.setAttribute('rotation', '0 0 0')
+      elem.setAttribute('position', '1.5 0 -5.167')
+      scene.appendChild(elem)
+    })
+})
+
+document.getElementById('ground').addEventListener('click', function (evt) {
+  // only teleport once the intro has been closed
+  if (intro.getAttribute('visible')) return
+
+  if (window.location.search.indexOf('teleporter') >= 0) {
+    teleporter.setAttribute('scale', '1 1 1')
+    teleporter.dispatchEvent(new CustomEvent('teleport'))
+  }
+
+  console.log('click ground', evt.detail.intersection.point)
+  var targetPos = evt.detail.intersection.point
+  setTimeout(function () { user.setAttribute('position', targetPos.x + ' 1.6 ' + targetPos.z) }, window.location.search.indexOf('teleporter') >= 0 ? 500 : 0)
+})
+
+document.getElementById('teleporterFadeOut').addEventListener('animationend', function () {
+  teleporter.setAttribute('scale', '0 0 0')
+})
+
+document.getElementById('player1').addEventListener('click', function () {
+  document.getElementById('day1').play()
+})
